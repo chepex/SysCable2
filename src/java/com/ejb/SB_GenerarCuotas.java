b9e6957407ca426af95a68277d2c5e902d994987
mmixco <mmixco@192.168.1.6>
2016-11-23 00:22:38 -0600

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	web/cliente/List.xhtml

--- src/java/com/ejb/SB_GenerarCuotas.java
+++ src/java/com/ejb/SB_GenerarCuotas.java
@@ -63,19 +63,7 @@ public class SB_GenerarCuotas {
                   }
         }    
             
-     /*
-        if(contrato.getPagoList().isEmpty()){
-            Calendar cal = Calendar.getInstance();    
-            cal.setTime(contrato.getFechainicio());        
-            vmes= String.valueOf(cal.get(Calendar.MONTH)+1);
-            vanio = String.valueOf(cal.get(Calendar.YEAR));
-        }else{
-            String  idP =pagoFacade.findPago(contrato);
-            int idPago = Integer.valueOf(idP);
-            Pago p =pagoFacade.find( idPago );      
-            vanio = p.getAniomes().substring(0,3);
-            vmes = p.getAniomes().substring(4,5);
-        }*/
+     
         
         System.out.println("anio --->"+vanio);
         System.out.println(" mes--->"+vmes);
